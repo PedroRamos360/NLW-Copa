221e58220c1e00ccb792b796389129a5b9348709
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-11-03 21:56:12 -0300

    finalização da aula 2 e início da aula 3

---
+++ server/prisma/seed.ts
@@ -0,0 +1,64 @@
+// Arquivo para popular o banco de dados para testes
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: "John Doe",
+            email: "john.doe@gmail.com",
+            avatarUrl: "github.com/PedroRamos360.png",
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: "Example Pool",
+            code: "BOL123",
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: "2022-11-03T12:00:00.389Z",
+            firstTeamCountryCode: "DE",
+            secondTeamCountryCode: "BR",
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: "2022-11-04T12:00:00.389Z",
+            firstTeamCountryCode: "BR",
+            secondTeamCountryCode: "AR",
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    })
+}
+
+main()
